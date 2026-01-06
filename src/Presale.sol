22a24393743da3ad1e9297b0bfa5bc52574faa77
Agustin <agustin.acosta@eestn1ee.edu.ar>
2026-01-06 02:45:21 -0300

    feat(presale): add presale contract with Foundry project setup
    Initialize Foundry-based Solidity project with presale functionality.
    Added project configuration, dependencies (forge-std, OpenZeppelin),
    and implemented a Presale contract supporting USDT/USDC payments
    with blacklist functionality and emergency withdrawal features.

---
+++ src/Presale.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Presale is Ownable {
+    using SafeERC20 for IERC20;
+
+    address public immutable usdtAddress;
+    address public immutable usdcAddress;
+    address public immutable fundsReceiverAddress;
+    uint256 public maxSellingAmount;
+    uint256[][3] public phases;
+    uint256 public startTime;
+    uint256 public endTime;
+    mapping(address => bool) public blacklistedAddresses;
+
+    /**
+     * @notice Initializes the contract with the initial configuration
+     * @param _usdtAddress The address of the USDT token
+     * @param _usdcAddress The address of the USDC token
+     * @param _fundsReceiverAddress The address that will receive the funds
+     * @param _maxSellingAmount The maximum amount allowed for sale per user
+     * @param _phases The configuration for the presale phases
+     * @param _startTime The timestamp when the presale starts
+     * @param _endTime The timestamp when the presale ends
+     */
+    constructor(
+        address _usdtAddress, 
+        address _usdcAddress, 
+        address _fundsReceiverAddress, 
+        uint256 _maxSellingAmount, 
+        uint256[][3] memory _phases,
+        uint256 _startTime,
+        uint256 _endTime
+    ) Ownable(msg.sender) {
+        usdtAddress = _usdtAddress;
+        usdcAddress = _usdcAddress;
+        fundsReceiverAddress = _fundsReceiverAddress;
+        maxSellingAmount = _maxSellingAmount;
+        phases = _phases;
+        startTime = _startTime;
+        endTime = _endTime;
+        require(startTime < endTime, "Invalid time range");
+    }
+    
+    /**
+     * @notice Adds an address to the blacklist
+     * @param _user The address to blacklist
+     */
+    function blacklist(address _user) external onlyOwner {
+        blacklistedAddresses[_user] = true;
+    } 
+
+    /**
+     * @notice Removes an address from the blacklist
+     * @param _user The address to unblacklist
+     */
+    function unblacklist(address _user) external onlyOwner {
+        blacklistedAddresses[_user] = false;
+    } 
+
+    /**
+     * @notice Allows users to buy tokens using stablecoins
+     * @param _amount The amount of stablecoins to spend
+     */
+    function buyWithStableCoin(uint256 _amount) external {
+        require(!blacklistedAddresses[msg.sender], "User is blacklisted");
+        require(_amount <= maxSellingAmount, "Amount exceeds max selling amount");
+        // Logic for token transfer and allocation goes here
+    }
+
+    /**
+     * @notice Withdraws ERC20 tokens in case of emergency
+     * @param _tokenAddress The address of the token to withdraw
+     * @param _amount The amount to withdraw
+     */
+    function emergencyERC20Withdraw(address _tokenAddress, uint256 _amount) external onlyOwner {
+        IERC20(_tokenAddress).safeTransfer(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Withdraws ETH in case of emergency
+     */
+    function emergencyETHWithdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "ETH transfer failed");
+    }
+
+
+}
+
+
